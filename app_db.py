2c9e9dcd4734e1f9c0866b62a66842aa7032e532
David Garcia <44665014+OXDavidXO@users.noreply.github.com>
2021-12-06 12:07:43 -0600

    Create app_db.py
    This is the database that is created to store information and to use throughout the website.

---
+++ app_db.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db'
+db = SQLAlchemy(app)
+
+
+class Projects(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    date_added = db.Column('Dated Added', db.DateTime, default = datetime.datetime.now)
+    title = db.Column('Project Name', db.String())
+    completed = db.Column('Completion Date', db.String())
+    description = db.Column('Decription', db.Text())
+    skills = db.Column('Skills', db.Text())
+    link = db.Column('Project Link', db.Text())
