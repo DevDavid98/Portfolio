efc2df1ffbd5e6116b25223ed31073c491d023f4
David Garcia <44665014+OXDavidXO@users.noreply.github.com>
2021-12-07 01:07:01 -0600

    Update app.py
    cleaned code and changed function names via pep8

--- app.py
+++ app.py
@@ -1,87 +1,95 @@
-from flask import (render_template, redirect, url_for, request)
-from app_db import db, Projects, app
-
-
-
-
-
-#home page 
+# Home page
 @app.route('/')
 def index():
+
     projects = Projects.query.all()
-    return render_template('index.html', projects = projects)
+    return render_template('index.html', projects=projects)
 
 
+# Add project form
+@app.route('/add', methods=['GET', 'POST'])
+def add():
 
-
-
-
-
-#form page 
-@app.route('/add project', methods = ['GET','POST'])
-#ITS NAME NOT ID
-def add_project():
     if request.form:
-        new_project = Projects(title = request.form['title'],
-                               completed = request.form['date'],
-                               description = request.form['desc'],
-                               skills = request.form['skills'],
-                               link = request.form['github'])
+
+        new_project = Projects(
+            title=request.form['title'],
+            completed=request.form['date'],
+            description=request.form['desc'],
+            skills=request.form['skills'],
+            link=request.form['github'])
+
         db.session.add(new_project)
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('new_project_form.html')
+
+    projects = Projects.query.all()
+
+    return render_template('new_project_form.html', projects=projects)
 
 
-
-
-
-
-
-
-
-# figure out detail route
 @app.route('/project/<id>')
 def project(id):
+
+    projects = Projects.query.all()
     project = Projects.query.get_or_404(id)
     project_skills = []
-    
+
     for skill in project.skills.split(","):
         project_skills.append(skill)
-        
-    return render_template('detail.html', project = project, project_skills = project_skills)
+
+    return render_template(
+        'detail.html',
+        project=project,
+        project_skills=project_skills,
+        projects=projects)
 
 
-
-
-
-
-
-
-
-@app.route('/edit/<id>', methods = ['GET','POST'])
+# Edit project form
+@app.route('/edit/<id>', methods=['GET', 'POST'])
 def edit(id):
+
     project = Projects.query.get(id)
+
     if request.form:
+
         project.title = request.form['title']
         project.completed = request.form['date']
         project.description = request.form['desc']
         project.skills = request.form['skills']
         project.link = request.form['github']
         db.session.commit()
+
         return redirect(url_for('index'))
-    return render_template('edit_project_form.html', project = project)
+
+    projects = Projects.query.all()
+
+    return render_template(
+        'edit_project_form.html',
+        project=project,
+        projects=projects)
 
 
+# Delete Project
 @app.route('/delete/<id>')
 def delete(id):
+
     project = Projects.query.get(id)
     db.session.delete(project)
     db.session.commit()
+
     return redirect(url_for('index'))
 
 
+# About me page
+@app.route('/about')
+def about():
+
+    projects = Projects.query.all()
+    return render_template('about.html', projects=projects)
+
+
 if __name__ == '__main__':
+
     db.create_all()
-    app.run(debug = True , port = 8000, host = '0.0.0.0')
-    
+    app.run(debug=True, port=8000, host='0.0.0.0')
